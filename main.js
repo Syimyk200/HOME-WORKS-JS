d02706b5295f991a5d1176c9d4d59c8ebcc26af6
Сыймык Маматкодыров <mamatkodyrov@gmail.com>
2025-01-19 01:07:54 +0300

    create hw17

---
+++ main.js
@@ -0,0 +1,42 @@
+// let i = 0;
+// while (i < 100) {
+//    i++;
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 ==0) console.log(i);
+// }
+
+// let sum = 0;
+// for (let i = 0; i <= 100; i++) {
+//   if(i % 2 ==0)
+//   sum = sum + i;
+//   console.log(i);
+// }
+// console.log(sum);
+
+// let a = 0;
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 !== 0) a = a + i;
+//   console.log(i);
+// }
+// console.log(a);
+
+// let a = "Duolingo Green";
+// let b = prompt("Hello");
+// let c = 0;
+// while (c <= a.length) {
+//   const d = a.charAt(c);
+//   if (d === b) {
+//     console.log(`yes ${c}`);
+//   }
+//   c++;
+// }
+
+// let a = "Duolingo Green";
+// let b = "e";
+// let c = a.length;
+// for (let d = 0; d <= c; c--) {
+//   console.log("yes e char");
+// }
